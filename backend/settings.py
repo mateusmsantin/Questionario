b5de9e9ccf66a9bc4f1ecad722f0b04f3ce7a928
Mateus Madail Santin <mateus.santin@gmail.com>
2026-02-27 13:03:46 -0300

    Configuração para deploy com django-environ

--- backend/settings.py
+++ backend/settings.py
@@ -106,14 +106,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # backend/settings.py
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
-    }
+    'default': env.db()
 }
 
 # Adicione isso no final do arquivo para o login por e-mail funcionar
