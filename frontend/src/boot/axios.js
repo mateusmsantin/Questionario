adf4923d8120fdb0686135bd66b956028bb0ccd7
Mateus Madail Santin <mateus.santin@gmail.com>
2026-02-28 08:55:48 -0300

    Ajustes para produção: CSRF_TRUSTED_ORIGINS e axios baseURL dinâmico

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,8 +1,12 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
+//const api = axios.create({
+//  baseURL: 'http://127.0.0.1:8000/api',
+//})
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: process.env.API_BASE_URL || '/api',
 })
 
 // Interceptor para garantir que o token seja pego do localStorage EM CADA requisição
